5cb083b6961681f8c20c4be401be5415c89471f5
Thomas Bühren <thomas.buehren@ferchau.com>
2024-04-19 12:52:35 +0200

    Explicitly configure Docker mounts as read-write

--- scripts/build-html.sh
+++ scripts/build-html.sh
@@ -4,8 +4,8 @@ set -e
 
 echo "Generate HTML Slides with Asciidoctor reveal.js"
 
-docker run -t --rm \
-    -v "$(pwd)":/documents/ \
+docker run --rm -t \
+    -v "$(pwd)":/documents/:rw \
     asciidoctor/docker-asciidoctor:1.69.0 \
         asciidoctor-revealjs --failure-level=WARN -v \
           fontbug.adoc
--- scripts/build-pdf.sh
+++ scripts/build-pdf.sh
@@ -5,11 +5,11 @@ set -e
 echo "Generate PDF from HTML Slides with decktape"
 
 docker run --rm -t \
-    -v "$(pwd)":/slides \
+    -v "$(pwd)":/slides/:rw \
     astefanutti/decktape:3.12.0 \
         fontbug.html fontbug.pdf
 
 docker run --rm -t \
-    -v "$(pwd)":/slides \
+    -v "$(pwd)":/slides/:rw \
     ghcr.io/thomas-ferchau/decktape:pr-1 \
         fontbug.html fontbug-workaround.pdf
