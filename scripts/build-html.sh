8c872789e678e1899077951fe7dfcdf4ea6d715c
Thomas Bühren <thomas.buehren@ferchau.com>
2024-04-19 12:35:22 +0200

    Remove input console when running the containers
    (does not work in GitHub Actions)

--- scripts/build-html.sh
+++ scripts/build-html.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Generate HTML Slides with Asciidoctor reveal.js"
 
-docker run -it --rm -u "$(id -u):$(id -g)" \
+docker run -t --rm -u "$(id -u):$(id -g)" \
     -v "$(pwd)":/documents/ \
     asciidoctor/docker-asciidoctor:1.69.0 \
         asciidoctor-revealjs --failure-level=WARN -v \
--- scripts/build-pdf.sh
+++ scripts/build-pdf.sh
@@ -4,12 +4,12 @@ set -e
 
 echo "Generate PDF from HTML Slides with decktape"
 
-docker run --rm -it -u "$(id -u):$(id -g)" \
+docker run --rm -t -u "$(id -u):$(id -g)" \
     -v "$(pwd)":/slides \
     astefanutti/decktape:3.12.0 \
         fontbug.html fontbug.pdf
 
-docker run --rm -it -u "$(id -u):$(id -g)" \
+docker run --rm -t -u "$(id -u):$(id -g)" \
     -v "$(pwd)":/slides \
     ghcr.io/thomas-ferchau/decktape:pr-1 \
         fontbug.html fontbug-workaround.pdf
