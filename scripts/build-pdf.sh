df855ce6cd62b9b0514b44ee99bcc9875dbd23d7
Thomas Bühren <thomas.buehren@ferchau.com>
2024-04-19 13:41:49 +0200

    Write PDF files to subdirectory
    (no write access to main directory in GitHub Actions)

--- scripts/build-pdf.sh
+++ scripts/build-pdf.sh
@@ -3,13 +3,14 @@
 set -e
 
 echo "Generate PDF from HTML Slides with decktape"
+mkdir -pv pdf
 
 docker run --rm -t \
     -v "$(pwd)":/slides/:rw \
     astefanutti/decktape:3.12.0 \
-        fontbug.html fontbug.pdf
+        fontbug.html pdf/fontbug.pdf
 
 docker run --rm -t \
     -v "$(pwd)":/slides/:rw \
     ghcr.io/thomas-ferchau/decktape:pr-1 \
-        fontbug.html fontbug-workaround.pdf
+        fontbug.html pdf/fontbug-workaround.pdf
